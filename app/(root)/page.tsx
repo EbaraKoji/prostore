a834d02bd4db48a85e6698072b076f3bb7fb8fdc
EbaraKoji <kojieva5270@gmail.com>
2025-09-27 19:04:20 +0900

    add not-found and loading page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const metadata = {
   title: 'Home',
 };
 
-const HomePage = () => {
+const HomePage = async () => {
+  // await delay(2000);
   return <div>Prostore</div>;
 };
 
