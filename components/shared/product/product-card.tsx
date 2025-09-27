b6dfa35d1101e15d7befd73c2f6efec151bf4fc8
EbaraKoji <kojieva5270@gmail.com>
2025-09-27 21:26:51 +0900

    add product price component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Product } from '@/db/sample-data';
 import Image from 'next/image';
 import Link from 'next/link';
+import { ProductPrice } from './product-price';
 
 interface Props {
   product: Product;
@@ -23,7 +24,7 @@ export const ProductCard = ({ product }: Props) => {
         <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">{product.price}</p>
+            <ProductPrice value={product.price} />
           ) : (
             <p className="text-destructive">Out of Stock</p>
           )}
