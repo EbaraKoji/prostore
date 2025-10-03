dc20e6603ebc6024c590cdd5fe3a1f3f0188d958
EbaraKoji <kojieva5270@gmail.com>
2025-10-03 16:21:20 +0900

    add useTransition in cart buttons

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -3,9 +3,10 @@
 import { Button } from '@/components/ui/button';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import type { CartItemSchema, CartSchema } from '@/types';
-import { Minus, Plus } from 'lucide-react';
+import { Minus, Plus, Loader } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
+import { useTransition } from 'react';
 
 interface Props {
   item: CartItemSchema;
@@ -13,62 +14,79 @@ interface Props {
 }
 
 export const AddToCart = ({ cart, item }: Props) => {
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
-    if (!res?.success) {
-      toast.custom((t) => (
-        <div className="flex space-x-4 items-center bg-red-200 border border-red-400 rounded-lg px-2 py-1">
-          <span className="text-sm text-red-700 font-semibold">{res?.message}</span>
-          <Button
-            onClick={() => toast.dismiss(t)}
-            variant="destructive"
-            className="px-1 text-xs cursor-pointer"
-          >
-            Close
-          </Button>
-        </div>
-      ));
-      return;
-    }
-    toast.success(res.message, {
-      action: {
-        label: 'Go To Cart',
-        onClick: () => {
-          router.push('/cart');
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      if (!res?.success) {
+        toast.custom((t) => (
+          <div className="flex space-x-4 items-center bg-red-200 border border-red-400 rounded-lg px-2 py-1">
+            <span className="text-sm text-red-700 font-semibold">{res?.message}</span>
+            <Button
+              onClick={() => toast.dismiss(t)}
+              variant="destructive"
+              className="px-1 text-xs cursor-pointer"
+            >
+              Close
+            </Button>
+          </div>
+        ));
+        return;
+      }
+      toast.success(res.message, {
+        action: {
+          label: 'Go To Cart',
+          onClick: () => {
+            router.push('/cart');
+          },
         },
-      },
+      });
     });
   };
 
   const existItem = cart && cart.items.find((cartItem) => cartItem.productId === item.productId);
   if (existItem === undefined) {
     return (
-      <Button className="w-full cursor-pointer" type="button" onClick={handleAddToCart}>
-        <Plus />
-        <span>Add To Cart</span>
+      <Button
+        className="w-full cursor-pointer"
+        type="button"
+        onClick={handleAddToCart}
+        disabled={isPending}
+      >
+        {isPending ? (
+          <Loader className="animate-spin" />
+        ) : (
+          <>
+            <Plus />
+            <span>Add To Cart</span>
+          </>
+        )}
       </Button>
     );
   }
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(existItem.productId);
-    if (!res?.success) {
-      toast.custom((t) => (
-        <div className="flex space-x-4 items-center bg-red-200 border border-red-400 rounded-lg px-2 py-1">
-          <span className="text-sm text-red-700 font-semibold">{res?.message}</span>
-          <Button
-            onClick={() => toast.dismiss(t)}
-            variant="destructive"
-            className="px-1 text-xs cursor-pointer"
-          >
-            Close
-          </Button>
-        </div>
-      ));
-      return;
-    }
-    toast.success(res.message);
+    startTransition(async () => {
+      const res = await removeItemFromCart(existItem.productId);
+      if (!res?.success) {
+        toast.custom((t) => (
+          <div className="flex space-x-4 items-center bg-red-200 border border-red-400 rounded-lg px-2 py-1">
+            <span className="text-sm text-red-700 font-semibold">{res?.message}</span>
+            <Button
+              onClick={() => toast.dismiss(t)}
+              variant="destructive"
+              className="px-1 text-xs cursor-pointer"
+              disabled={isPending}
+            >
+              Close
+            </Button>
+          </div>
+        ));
+        return;
+      }
+      toast.success(res.message);
+    });
   };
 
   return (
@@ -78,12 +96,19 @@ export const AddToCart = ({ cart, item }: Props) => {
         variant="outline"
         className="cursor-pointer"
         onClick={handleRemoveFromCart}
+        disabled={isPending}
       >
-        <Minus />
+        {isPending ? <Loader className="animate-spin" /> : <Minus />}
       </Button>
       <span className="px-2">{existItem.qty}</span>
-      <Button type="button" variant="outline" className="cursor-pointer" onClick={handleAddToCart}>
-        <Plus />
+      <Button
+        type="button"
+        variant="outline"
+        className="cursor-pointer"
+        onClick={handleAddToCart}
+        disabled={isPending}
+      >
+        {isPending ? <Loader className="animate-spin" /> : <Plus />}
       </Button>
     </div>
   );
